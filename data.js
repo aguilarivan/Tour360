e3e7018596f6407960992621243d1b53542d0d5d
aguilarivan <ivanaguilarnardin@gmail.com>
2024-07-23 19:45:21 -0300

    add

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20240723_192117_632",
+      "name": "20240723_192117_632",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.8222198926434707,
+        "pitch": 0.8245654494567098,
+        "fov": 1.3311494768449008
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.47778497477689186,
+          "pitch": 0.2796735384818234,
+          "rotation": 0,
+          "target": "1-20240723_192746_890"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-20240723_192746_890",
+      "name": "20240723_192746_890",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.97959112957413,
+        "pitch": 0.1406355742380292,
+        "fov": 1.3311494768449008
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9722538812378367,
+          "pitch": 0.13596806506125958,
+          "rotation": 0,
+          "target": "0-20240723_192117_632"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
